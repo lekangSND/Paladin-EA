5e3335ce6b90cb04d5ffa99ff45ece47b64e021a
WangyanhuiRea <SESA378106@se.com>
2020-03-27 12:38:11 +0800

    add ea advanced incident monitor

---
+++ src/featureModule/index.ts
@@ -0,0 +1,28 @@
+import * as config from "../prodConfig";
+import * as puppeteer from "puppeteer";
+import { getLogger } from "log4js";
+import Customer from "./customer";
+import contentPage from "./contentPage";
+import { sysMgmtBtnClass } from "./config";
+import sysMgmtPage from "./systemManagementPage";
+const logger = getLogger("eaModule");
+
+const switchToSysMgmtPage = async (page: puppeteer.Page) => {
+  logger.info("switch to System Management Page");
+  var sysMgmtBtn = await page.$(sysMgmtBtnClass);
+  await Promise.all([page.waitForNavigation(), sysMgmtBtn.click()]);
+};
+
+export default async (config: config.Config, page: puppeteer.Page) => {
+  logger.info("Into EA-Main");
+
+  await Customer(config, page);
+
+  await contentPage(config, page);
+
+  await switchToSysMgmtPage(page);
+
+  await sysMgmtPage(config, page);
+
+  logger.info("leave EA-Main");
+};
