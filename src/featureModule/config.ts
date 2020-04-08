3a34c20be9c498cdae959074627d8c105070b4b6
WangyanhuiRea <SESA378106@se.com>
2020-04-08 10:34:08 +0800

    add 数据分析

--- src/featureModule/config.ts
+++ src/featureModule/config.ts
@@ -16,53 +16,69 @@ export const MENU_NAV_TIMEOUT = 15;
 
 export const contentPageConfig: Array<MenuConfigObject> = [
   {
+    key: "dataanalysis",
+    name: "数据分析",
+    children: [
+      {
+        key: "data_analysis",
+        name: "数据分析",
+        validClass: ".jazz-new-folder-leftpanel-header",
+      },
+      {
+        key: "input_data",
+        name: "录入数据",
+        validClass: ".jazz-input-data-content-panel-header",
+      },
+    ],
+  },
+  {
     key: "customersetting",
     name: "客户配置",
     children: [
       {
         key: "ptag",
         name: "计量数据P",
-        validClass: ".pop-manage-detail-header-name"
+        validClass: ".pop-manage-detail-header-name",
       },
       {
         key: "vtag",
         name: "计量数据V",
-        validClass: ".jazz-tag-leftpanel-header-item"
+        validClass: ".jazz-tag-leftpanel-header-item",
       },
       {
         key: "tag_import_log",
         name: "配置导入日志",
-        validClass: ".jazz-template-list"
+        validClass: ".jazz-template-list",
       },
       {
         key: "hierarchy",
         name: "层级节点配置",
-        validClass: ".jazz-tag-leftpanel-header-item"
+        validClass: ".jazz-tag-leftpanel-header-item",
       },
       {
         key: "import_log",
         name: "配置导入日志",
-        validClass: ".jazz-template-list"
+        validClass: ".jazz-template-list",
       },
       {
         key: "virtuat_gateway",
         name: "虚拟网关",
         validClass: ".title.customerName",
-        outer: true
+        outer: true,
       },
       {
         key: "data_validation",
         name: "数据质量维护",
-        validClass: ".data-quality-maintenance-filter-time"
+        validClass: ".data-quality-maintenance-filter-time",
       },
 
       {
         key: "kpi_cycle",
         name: "指标计算周期",
-        validClass: ".header-bar"
-      }
-    ]
-  }
+        validClass: ".header-bar",
+      },
+    ],
+  },
   // {
   //   key: "dashborad",
   //   name: "Dashboard",
@@ -81,24 +97,24 @@ export const sysMgmtPageConfig: Array<MenuConfigObject> = [
       {
         key: "work_day",
         name: "工休日",
-        validClass: ".jazz-calendar-title"
+        validClass: ".jazz-calendar-title",
       },
       {
         key: "work_time",
         name: "工作时间",
-        validClass: ".jazz-calendar-title"
+        validClass: ".jazz-calendar-title",
       },
       {
         key: "hc_season",
         name: "冷暖季",
-        validClass: ".jazz-calendar-title"
+        validClass: ".jazz-calendar-title",
       },
       {
         key: "day_night",
         name: "昼夜时间",
-        validClass: ".jazz-calendar-title"
-      }
-    ]
+        validClass: ".jazz-calendar-title",
+      },
+    ],
   },
   {
     key: "Conversion",
@@ -107,19 +123,19 @@ export const sysMgmtPageConfig: Array<MenuConfigObject> = [
       {
         key: "price",
         name: "价格",
-        validClass: ".pop-manage-detail-header-name.jazz-header"
+        validClass: ".pop-manage-detail-header-name.jazz-header",
       },
       {
         key: "carbon",
         name: "碳排放",
-        validClass: ".jazz-carbon-detail-header-name.jazz-carbon-header"
-      }
-    ]
+        validClass: ".jazz-carbon-detail-header-name.jazz-carbon-header",
+      },
+    ],
   },
   {
     key: "customer",
     name: "客户管理",
-    validClass: ".pop-manage-detail-header-name.jazz-header"
+    validClass: ".pop-manage-detail-header-name.jazz-header",
   },
   {
     key: "UserManagment",
@@ -128,13 +144,13 @@ export const sysMgmtPageConfig: Array<MenuConfigObject> = [
       {
         key: "user",
         name: "用户管理",
-        validClass: ".pop-manage-detail-header-name"
+        validClass: ".pop-manage-detail-header-name",
       },
       {
         key: "role",
         name: "功能权限角色",
-        validClass: ".pop-manage-detail-header-name.jazz-header"
-      }
-    ]
-  }
+        validClass: ".pop-manage-detail-header-name.jazz-header",
+      },
+    ],
+  },
 ];
